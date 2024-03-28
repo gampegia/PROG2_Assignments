5043c7561986cf0b601152a4936eaff5b1324657
Sunkarr <126823904+Sunkarr@users.noreply.github.com>
2024-03-28 18:11:42 +0100

    Prog

--- P2_1_Inheritance in OOP/BankApplication.py
+++ P2_1_Inheritance in OOP/BankApplication.py
@@ -174,49 +174,6 @@ class BankApplication:
             else:
                 print("Invalid choice")
 
-
-class TaxReport:
-    """
-    A class to generate tax reports for accounts.
-
-    Attributes:
-        accounts (list): A list of account instances.
-
-    Methods:
-        total_balance_savings(): Returns the total balance of all savings accounts.
-        total_balance_youth(): Returns the total balance of all youth accounts.
-    """
-
-    def __init__(self, accounts):
-        self.accounts = accounts
-
-    def total_balance_savings(self):
-        """
-        Returns the total balance of all savings accounts.
-
-        Returns:
-            float: The total balance of all savings accounts.
-        """
-        balance_savings = 0
-        for account in self.accounts:
-            if account.type == "savings":
-                balance_savings += account.get_amount()
-        return balance_savings
-
-    def total_balance_youth(self):
-        """
-        Returns the total balance of all youth accounts.
-
-        Returns:
-            float: The total balance of all youth accounts.
-        """
-        balance_youth = 0
-        for account in self.accounts:
-            if account.type == "youth":
-                balance_youth += account.get_amount()
-        return balance_youth
-
-
 # Instantiate the BankApplication class and run the application
 if __name__ == "__main__":
     bank_app = BankApplication()
