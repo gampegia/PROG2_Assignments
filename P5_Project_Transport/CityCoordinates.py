5cbe4be45f52f571e3b7c5e82d3fa9ebc73cb85d
Sunkarr <126823904+Sunkarr@users.noreply.github.com>
2024-05-04 16:55:55 +0200

    Prog

--- P5_Project_Transport/CityCoordinates.py
+++ P5_Project_Transport/CityCoordinates.py
@@ -2,8 +2,10 @@ import requests
 
 
 def get_coordinates(city_name, country_name):
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
+                             '(KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'}
     url = f"https://nominatim.openstreetmap.org/search?city={city_name}&country={country_name}&format=json"
-    response = requests.get(url)
+    response = requests.get(url, headers=headers)
     if response.status_code == 200 and response.content:
         data = response.json()
         if data:
