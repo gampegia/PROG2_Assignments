432fb9f77dabc2ffac23b6084e2f4c103483f87a
Gwendoline Vocat <gwendi.vocat@gmail.com>
2024-05-04 14:52:03 +0200

    feat: Update city and country names in Cities.py
    The city and country names in the `Cities.py` file have been updated to use consistent spelling and formatting. This ensures that the city and country names match the input format expected by the `CityCoordinates` class.

--- P5_Project_Transport/Cities.py
+++ P5_Project_Transport/Cities.py
@@ -3,8 +3,8 @@ from CityCoordinates import CityCoordinates
 
 # Define your data
 data = {
-    "City": ['Altach', 'Altstätten', 'Arbon', 'Au', 'Balzers', 'Basel', 'Bregenz', 'Buchs', 'Diepoldsau', 'Dornbirn', 'Eichberg', 'Feldkirch', 'Friedrichshafen', 'Gams', 'Haag', 'Heerbrugg', 'Koblach', 'Konstanz', 'Kreuzlingen', 'Kriessern', 'Küssaberg', 'Lindau', 'Lörrach', 'Lustenau', 'Mäder', 'Montlingen', 'Mulhouse', 'Oberriet', 'Rankweil', 'Rheinfelden', 'Romanshorn', 'Rorschach', 'Rüthi', 'Rüthi', 'Sankt-Ludwig', 'Sankt-Gallen', 'Sargans', 'Schaan', 'Schaffhausen', 'Sevelen', 'Singen', 'St.Margrethen', 'Überlingen', 'Vaduz', 'Waldshut-Tiengen', 'Weil-am-Rhein', 'Widnau', 'Mailand', 'Como'],
-    "Country": ['Österreich', 'Schweiz', 'Schweiz', 'Schweiz', 'Liechtenstein', 'Schweiz', 'Österreich', 'Schweiz', 'Schweiz', 'Österreich', 'Schweiz', 'Österreich', 'Deutschland', 'Schweiz', 'Schweiz', 'Schweiz', 'Österreich', 'Deutschland', 'Schweiz', 'Schweiz', 'Deutschland', 'Deutschland', 'Deutschland', 'Österreich', 'Österreich', 'Schweiz', 'Frankreich', 'Schweiz', 'Österreich', 'Schweiz', 'Schweiz', 'Schweiz', 'Schweiz', 'Liechtenstein', 'Frankreich', 'Schweiz', 'Schweiz', 'Liechtenstein', 'Schweiz', 'Schweiz', 'Deutschland', 'Schweiz', 'Deutschland', 'Liechtenstein', 'Deutschland', 'Deutschland', 'Schweiz', 'Italien', 'Italien']
+    "City": ['Altach', 'Altstaetten', 'Arbon', 'Au', 'Balzers', 'Basel', 'Bregenz', 'Buchs', 'Diepoldsau', 'Dornbirn', 'Eichberg', 'Feldkirch', 'Friedrichshafen', 'Gams', 'Haag', 'Heerbrugg', 'Koblach', 'Konstanz', 'Kreuzlingen', 'Kriessern', 'Kuessaberg', 'Lindau', 'Loerrach', 'Lustenau', 'Maeder', 'Montlingen', 'Mulhouse', 'Oberriet', 'Rankweil', 'Rheinfelden', 'Romanshorn', 'Rorschach', 'Rueti', 'Rueti', 'Sankt-Ludwig', 'Sankt-Gallen', 'Sargans', 'Schaan', 'Schaffhausen', 'Sevelen', 'Singen', 'St.Margrethen', 'Ueberlingen', 'Vaduz', 'Waldshut-Tiengen', 'Weil-am-Rhein', 'Widnau', 'Mailand', 'Como', 'Pontarlier', 'Morteau'],
+    "Country": ['Oesterreich', 'Schweiz', 'Schweiz', 'Schweiz', 'Liechtenstein', 'Schweiz', 'Oesterreich', 'Schweiz', 'Schweiz', 'Oesterreich', 'Schweiz', 'Oesterreich', 'Deutschland', 'Schweiz', 'Schweiz', 'Schweiz', 'Oesterreich', 'Deutschland', 'Schweiz', 'Schweiz', 'Deutschland', 'Deutschland', 'Deutschland', 'Oesterreich', 'Oesterreich', 'Schweiz', 'Frankreich', 'Schweiz', 'Oesterreich', 'Schweiz', 'Schweiz', 'Schweiz', 'Schweiz', 'Liechtenstein', 'Frankreich', 'Schweiz', 'Schweiz', 'Liechtenstein', 'Schweiz', 'Schweiz', 'Deutschland', 'Schweiz', 'Deutschland', 'Liechtenstein', 'Deutschland', 'Deutschland', 'Schweiz', 'Italien', 'Italien', 'Frankreich', 'Frankreich']
 }
 df = pd.DataFrame(data)
 
@@ -16,11 +16,11 @@ df['Longitude'] = None
 for index, row in df.iterrows():
     city = row['City']
     country = row['Country']
-    city_country = f"{city}-{country}"
+    city_country = f"{city},{country}"
     coordinates = CityCoordinates(city_country).get_city_coordinates()
-    df.loc[index, 'Latitude'] = coordinates[0]
-    df.loc[index, 'Longitude'] = coordinates[1]
+    if coordinates:
+        df.loc[index, 'Latitude'] = coordinates[0]
+        df.loc[index, 'Longitude'] = coordinates[1]
 
-
-df.to_csv('CityCoordinates.csv', index=False)
+print(df)
 
