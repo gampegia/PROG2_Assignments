5e8f32943403a2a23bfd1a05846325a61edd3374
Gwendoline Vocat <gwendi.vocat@gmail.com>
2024-05-14 13:09:48 +0200

    asdf

--- P5_Project_Transport/Menu.py
+++ P5_Project_Transport/Menu.py
@@ -1,14 +1,21 @@
+import pandas as pd
 import TrainConnection
-
+import Blacklist
 
 class TrainConnectionMenu:
     def __init__(self):
-        self.start = None
-        self.destination = None
+        self.start_city = None
+        self.destination_city = None
+        self.destination_country = None
 
     def input_route(self):
-        self.start = input("Enter the start station: ")
-        self.destination = input("Enter the destination station: ")
+        self.start_city = input("Enter the start city: ")
+        self.destination_city = input("Enter the destination city: ")
+        self.destination_country = input("Enter the destination country: ")
+
+    def get_train_website(self):
+        website = self.df.loc[self.df['Country'] == self.destination_country, 'Website'].values[0]
+        return website
 
     def display_connection(self):
         if self.start and self.destination:
