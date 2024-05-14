dba3bbb15e8d5a25cf8303f70b1e76f441e2eaff
Gwendoline Vocat <gwendi.vocat@gmail.com>
2024-05-14 14:50:25 +0200

    Refactor TrainConnectionMenu class for better variable naming

--- P5_Project_Transport/Menu.py
+++ P5_Project_Transport/Menu.py
@@ -5,14 +5,14 @@ import countrymapping
 
 class TrainConnectionMenu:
     def __init__(self):
-        self.start = None
-        self.destination = None
+        self.start_city = None
+        self.destination_city = None
         self.destination_country = None
         self.df = pd.read_csv('countrytrainmapping.csv')
 
     def input_route(self):
-        self.start = input("Enter the start city: ")
-        self.destination = input("Enter the destination city: ")
+        self.start_city = input("Enter the start city: ")
+        self.destination_city = input("Enter the destination city: ")
         self.destination_country = (input("Enter the destination country: ")).lower()
 
     def get_train_website(self):
@@ -20,10 +20,10 @@ class TrainConnectionMenu:
         print(f"Visit {website} for more information.")
 
     def display_connection(self):
-        if self.start and self.destination:
-            print(f"Train connection from {self.start} to {self.destination}")
+        if self.start_city and self.destination_city:
+            print(f"Train connection from {self.start_city} to {self.destination_city}")
             train_connection = TrainConnection.TrainConnection()
-            connection = train_connection.TrainConnectionDownloader(self.start, self.destination)
+            connection = train_connection.TrainConnectionDownloader(self.start_city, self.destination_city)
             #print(connection) # DEBUG Statement
             train_connection.display_next_connection(connection)
         else:
