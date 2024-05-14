209492d6692e8d9181561187b8115759ea9f80d3
gampegia <148438859+gampegia@users.noreply.github.com>
2024-05-14 20:06:28 +0200

    added check blacklist

--- P5_Project_Transport/Menu.py
+++ P5_Project_Transport/Menu.py
@@ -3,6 +3,7 @@ import TrainConnection
 import countrymapping
 import CityCoordinates
 import Calculator
+import Blacklist
 
 class TrainConnectionMenu:
     def __init__(self):
@@ -10,6 +11,7 @@ class TrainConnectionMenu:
         Initialize the TrainConnectionMenu class with default values.
         """
         self.start_city = None
+        self.start_country = None
         self.destination_city = None
         self.destination_country = None
         try:
@@ -24,6 +26,7 @@ class TrainConnectionMenu:
         Get input from the user for start city, destination city, and destination country.
         """
         self.start_city = input("Enter the start city: ")
+        self.start_country = input("Enter the start country: ").lower()
         self.destination_city = input("Enter the destination city: ")
         self.destination_country = input("Enter the destination country: ").lower()
 
@@ -32,7 +35,6 @@ class TrainConnectionMenu:
         Get the website for train connections based on the destination country.
         """
         try:
-            self.destination_country = self.destination_country.lower()
             website = self.df.loc[self.df['Country'] == self.destination_country, 'Website'].values[0]
             msg_visit_country_website = f"Visit {website} for more information."
             TrainConnectionMenu.set_console_output(msg_visit_country_website)
@@ -55,37 +57,45 @@ class TrainConnectionMenu:
         Display the train connection from the start city to the destination city.
         If no direct connection is found, suggest the nearest station and find connections to it.
         """
-        if self.start_city and self.destination_city:
-            try:
-                msg_from_to = f"Train connection from {self.start_city} to {self.destination_city}"
-                TrainConnectionMenu.set_console_output(msg_from_to)
+        if self.start_city and self.destination_city and self.destination_country:
+            blacklist_checker = Blacklist.Blacklist()
+            if blacklist_checker.check_blacklist(self.start_city, self.start_country, self.destination_city, self.destination_country):
+                msg_blacklist = (f"The Route from {self.start_city} to "
+                                 f"{self.destination_city} is on the blacklist, try another one.")
+                TrainConnectionMenu.set_console_output(msg_blacklist)
+            else:
+                try:
+                    msg_from_to = f"Train connection from {self.start_city} to {self.destination_city}"
+                    TrainConnectionMenu.set_console_output(msg_from_to)
 
-                train_connection = TrainConnection.TrainConnection()
-                connection = train_connection.TrainConnectionDownloader(self.start_city + " " + self.destination_country, self.destination_city)
+                    train_connection = TrainConnection.TrainConnection()
+                    connection = train_connection.TrainConnectionDownloader(
+                        self.start_city + " " + self.start_country, self.destination_city)
 
-                if connection:
-                    train_connection.display_next_connection(connection)
-                else:
-                    calculator = Calculator.Calculator()
-                    calculator.set_start_city(self.start_city, 'switzerland')
-                    calculator.set_destination_city(self.destination_city, self.destination_country)
-                    calculator.__post_init__()
+                    if connection:
+                        train_connection.display_next_connection(connection)
+                    else:
+                        calculator = Calculator.Calculator()
+                        calculator.set_start_city(self.start_city, self.start_country)
+                        calculator.set_destination_city(self.destination_city, self.destination_country)
+                        calculator.__post_init__()
 
-                    next_station = calculator.choose_transfer_station()
-                    msg_not_found = f"No direct connection found. The nearest station to {self.destination_city} is {next_station}."
-                    msg_finding_alternative = f"Finding connection to {next_station}..."
-                    TrainConnectionMenu.set_console_output(msg_not_found)
-                    TrainConnectionMenu.set_console_output(msg_finding_alternative)
+                        next_station = calculator.choose_transfer_station()
+                        msg_not_found = (f"No direct connection found. The nearest station to "
+                                         f"{self.destination_city} is {next_station}.")
+                        msg_finding_alternative = f"Finding connection to {next_station}..."
+                        TrainConnectionMenu.set_console_output(msg_not_found)
+                        TrainConnectionMenu.set_console_output(msg_finding_alternative)
 
-                    connection_to_next_station = train_connection.TrainConnectionDownloader(self.start_city, next_station)
-                    train_connection.display_next_connection(connection_to_next_station)
-                    train_connection.display_next_connection(connection)
-            except Exception as e:
-                msg_invalide_route =f"An unexpected error occurred while displaying the connection: {e}"
-                TrainConnectionMenu.set_console_output(msg_invalide_route)
+                        connection_to_next_station = train_connection.TrainConnectionDownloader(self.start_city, next_station)
+                        train_connection.display_next_connection(connection_to_next_station)
+                        train_connection.display_next_connection(connection)
+                except Exception as e:
+                    msg_invalid_route = f"An unexpected error occurred while displaying the connection: {e}"
+                    TrainConnectionMenu.set_console_output(msg_invalid_route)
         else:
-            msg_invalide_route = "Please input a valid route first."
-            TrainConnectionMenu.set_console_output(msg_invalide_route)
+            msg_invalid_route = "Please input a valid route first."
+            TrainConnectionMenu.set_console_output(msg_invalid_route)
 
 
 if __name__ == "__main__":
